f3a3e6da850e68edbbcfa18fea8a083ed43bc4da
filippospr <39134175+filippospr@users.noreply.github.com>
2022-01-04 12:40:21 +0200

    Update node.c

--- lab 4/sclab4/node.c
+++ lab 4/sclab4/node.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <string.h>
-#include<stdbool.h>
-#include<stdlib.h>
 #include "node.h"
+#include <stdlib.h>
+#include<stdbool.h>
 
 int connectcosts[NUMNODES][NUMNODES];
+int msg_sent=0;
+
 
 /* Setup the network costs */
 void initcosts() {
 	static int initialized = 0;
 	if (!initialized) {
-
 		/* initialize by hand since not all compilers allow array initilization */
 		connectcosts[0][1] = connectcosts[1][0] = 1; // x0
 		connectcosts[1][2] = connectcosts[2][1] = 1; // x1
@@ -41,32 +42,34 @@ void initcosts() {
  */
 void initRT(Node* n) {
 	int i;
-	RtPkt * packet=(RtPkt*)malloc(sizeof(RtPkt));
-	packet->sourceid=n->id;
-	
 
-	for (i = 0; i < NUMNODES; i++){
+	RtPkt * packet=(RtPkt *)malloc(sizeof(RtPkt));
+	packet->sourceid=n->id;
+
+	for(i=0;i<NUMNODES;i++){
 		n->rt.cost[i]=connectcosts[n->id][i];
+		packet->mincost[i]=connectcosts[n->id][i];
+
+		//init next hop in case not connected
 		if(n->rt.cost[i]==999){
-			n->rt.nexthop[i]=999;
+			n->rt.nexthop[i]=999;	
 		}
 		else{
-			n->rt.nexthop[i]=i;
+			n->rt.nexthop[i]=i;	
 		}
-		packet->mincost[i]=n->rt.cost[i];
-	}
-	
 
-	//send update to all neighbours
+	}
+
+
+
+
 	for(i=0;i<NUMNODES;i++){
-		if(connectcosts[n->id][i]!=999){
-			if(i!=n->id){
-				packet->destid=i;
-				tolayer2(*packet);
-			}
+		if(i!=n->id && connectcosts[n->id][i]!=999){
+			packet->destid=i;
+			tolayer2(*packet);
+			msg_sent+=1;			
 		}
 	}
-
 
 
 
@@ -79,37 +82,34 @@ void initRT(Node* n) {
  * katallhlo RtPkt se oloys toys geitonikoys komboys toy node.
  */
 void updateRT(Node* n, RtPkt* rcvdpkt) {
-
 	int i;
-	bool changes=false;
-	RtPkt * packet=(RtPkt*)malloc(sizeof(RtPkt));	
+	bool changes;
+
+	changes=false;
+
+	RtPkt * packet=(RtPkt *)malloc(sizeof(RtPkt));
 	packet->sourceid=n->id;
-	
 
 	for(i=0;i<NUMNODES;i++){
-		if(rcvdpkt->sourceid!=i){
-			if(n->rt.cost[rcvdpkt->sourceid] + rcvdpkt->mincost[i] < n->rt.cost[i]){
-				n->rt.nexthop[i]=n->rt.nexthop[rcvdpkt->sourceid];
-				n->rt.cost[i]=n->rt.cost[rcvdpkt->sourceid] + rcvdpkt->mincost[i];
-				changes=true;
-			}
+		if(n->rt.cost[rcvdpkt->sourceid]+rcvdpkt->mincost[i]<n->rt.cost[i]){
+			n->rt.cost[i]=n->rt.cost[rcvdpkt->sourceid]+rcvdpkt->mincost[i];
+			n->rt.nexthop[i]=n->rt.nexthop[rcvdpkt->sourceid];
+			changes=true;
 		}
 		packet->mincost[i]=n->rt.cost[i];
 	}
 
-
-
 	if(changes==true){
 		for(i=0;i<NUMNODES;i++){
-			if(n->rt.cost[i]!=999){
-				if(i!=n->id){
-						packet->destid=i;
-						tolayer2(*packet);
-				}
-
+			if(n->rt.nexthop[i]==i){
+				packet->destid=i;
+				tolayer2(*packet);
+				msg_sent+=1;
 			}
 		}
 	}
+
+
 }
 
 /**
@@ -120,11 +120,13 @@ void updateRT(Node* n, RtPkt* rcvdpkt) {
  */
 void printRT(Node* n) {
 	int i;
-	printf("--------- Node %d-----------\n",n->id);
-	for (i = 0; i < NUMNODES; i++)
-	{
-		printf("Node :%d to node:%d cost is %d next hop is %d \n",n->id,i,n->rt.cost[i],n->rt.nexthop[i]);
+	printf("Routing table of node %d \n",n->id);
+	printf("Destination|Cost|Nexthop\n");
+	for(i=0;i<NUMNODES;i++){
+		if(i!=n->id){
+			printf("%d |%d | %d \n", i,n->rt.cost[i],n->rt.nexthop[i]);
+		}
 	}
-	printf("----------------------------\n");
+	printf("Total messages=%d\n",msg_sent);
 }
 
